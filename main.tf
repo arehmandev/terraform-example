44779035e121745b40c7b4cfce13b2240eadeb71
arehmandev <arehma@outlook.com>
2021-07-17 22:00:08 +0100

    Added all contents

---
+++ main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.13.0"
+    }
+  }
+}
+
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.latest
+  name  = "tutorial"
+  ports {
+    internal = 80
+    external = 8000
+  }
+}
+
+output "docker-id" {
+  value = docker_container.nginx.id
+  description = "docker image id"
+}
+
+output "docker-name" {
+  value = docker_container.nginx.name
+  description = "docker image name"
+}
